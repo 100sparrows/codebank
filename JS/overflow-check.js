40028ef0e604abe1e185850bafaf6a7d102c1b58
Lou <louise@67degrees.co.uk>
2025-04-16 15:55:39 +0100

    Adds code for showing full text in element when some has been hidden

---
+++ JS/overflow-check.js
@@ -0,0 +1,46 @@
+// Overflow boolean checker
+const elements = document.querySelectorAll(
+  ".vehicle--full .vehicle__header-third .vehicle__description"
+);
+console.error("Elements: ", elements);
+if (elements.length > 0) {
+  elements.forEach((el) => {
+    if (el.scrollHeight > el.clientHeight) {
+      const readMoreLink = document.querySelector(".vehicle__description-link");
+      readMoreLink.classList.remove("hide");
+    }
+  });
+}
+
+
+
+@mixin data-link{
+	text - align: right;
+	padding-top:2.8em;
+	background: linear-gradient(0deg,$secondary-color 0%, rgba($secondary-color,0.75) 100%);
+	position: relative;
+	margin-top:-1em;
+  @include breakpoint(xxlarge) {
+    margin-right: - rem-calc(55);
+  }
+  a {
+    color: $primary-color;
+    font-size: rem-calc(14);
+    font-weight: $global-weight-semi-bold;
+    text-decoration: underline;
+    text-transform: uppercase;
+    span {
+      position: relative;
+      top: rem-calc(5);
+      height: rem-calc(40);
+      width: rem-calc(40);
+      margin-left: rem-calc(10);
+      font-size: rem-calc(18);
+      line-height: rem-calc(40);
+      border-radius: 50%;
+      background: $white;
+      color: $black;
+      text-align: center;
+    }
+  }
+}
